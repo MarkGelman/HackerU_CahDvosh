001fa811f908d67e9af1628950c847946fbecae6
Mark <markgora1974@gmail.com>
2023-10-04 19:36:11 +0300

    statr_to_do_list

--- My_Project/GameCenter/GameCenter/TicTocToe.xaml.cs
+++ My_Project/GameCenter/GameCenter/TicTocToe.xaml.cs
@@ -15,11 +15,11 @@ using System.Windows.Shapes;
 
 namespace GameCenter
 {
-    /// <summary>
-    /// Interaction logic for TicTocToe.xaml
-    /// </summary>
+    
     public partial class TicTocToe : Page
     {
+        bool turn = true; //true = x turn; false = o turn
+        int turn_count = 0;
         public TicTocToe()
         {
             InitializeComponent();
@@ -27,7 +27,47 @@ namespace GameCenter
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Button b = (Button)sender;
+            b.Content = turn ? "X" : "O";
 
+            
+
+            turn = !turn;
+            b.IsEnabled = false;
+
+            checkForWinner();
+                
+        }
+
+        private void checkForWinner()
+        {
+            bool isWinner = false;
+
+            //Vertical cheks and event content of all buttons is empty
+            if((A1.Content == A2.Content) &&(A2.Content == A3.Content) && (!A1.IsEnabled))
+                isWinner = true;
+            else if ((B1.Content == B2.Content) && (B2.Content == B3.Content) && (!B1.IsEnabled))
+                isWinner = true;
+            else if ((C1.Content == C2.Content) && (C2.Content == C3.Content) && (!C1.IsEnabled))
+                isWinner = true;
+
+            //dioganal cheks
+            if(!B2.IsEnabled)
+                if ((A1.Content == B2.Content) && (B2.Content == C3.Content))
+                    isWinner = true;
+                else if ((A3.Content == B2.Content) && (B2.Content == C1.Content))
+                    isWinner = true;
+
+
+            //Message about winner
+            if(isWinner)
+            {
+                String winner = "";
+                winner = turn? "O" : "X";
+
+                MessageBox.Show(winner + "Wins!", "Yhoooooo!");
+            }
+   
         }
     }
 }
---
+++ My_Project/GameCenter/TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TodoApp.Models;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    { 
+
+        private BindingList<TodoModel> _todoDataList;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoDataList = new BindingList<TodoModel>()
+            {
+                new TodoModel {Text="test"},
+                new TodoModel(){Text="djsad"}
+            };
+
+            dgTodoList.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+        }
+
+        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if(e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+
+            }
+        }
+    }
+}
