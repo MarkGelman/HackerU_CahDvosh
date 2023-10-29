309df53c3bce8c8da0f3863804c668fa27a995a6
Gelman Mark <markgora1974@gmail.com>
2023-10-29 03:15:06 +0200

    SlideShow_Connect

---
+++ My_Project/Project_GameCenter/My_GameCenter_Projects/My_Launcher/My_Launcher/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using My_Launcher.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Launcher.ViewModel
+{
+    class MainViewModel
+    {
+        SlideShow files = new SlideShow();
+        public void SlideShow()
+        {
+            while(true)
+            {
+                foreach(var slide in files.slides)
+                {
+
+                }
+            }
+
+        }
+    }
+}
